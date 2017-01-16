19f2c05dfeb42d4cf19431c9d6e62cd766e01f9e
Chelsea Midgette <chelseamidgette@Chelseas-MacBook-Pro.local>
2017-01-16 12:11:20 -0800

    new commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+//final calculation
+function monthlyPayment(){
+
+	var interestRate = parseFloat(document.getElementById("rate").value);
+	var loanBalance = parseFloat(document.getElementById("balance").value);
+	var loanTerm = parseFloat(document.getElementById("term").value);
+	var paymentPeriod = parseFloat(document.getElementById("period").value);
+
+	// monthly interest rate
+	var monthlyInterestRate = (interestRate / 100) / paymentPeriod;	 
+
+	// number of payments
+	var numberOfPayments = (loanTerm * paymentPeriod);
+
+	//compounded interest rate
+	var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments);
+
+	//interest quotient
+	var interestQuotient = ((monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1));
+
+	document.getElementById("result").innerHTML = "Your monthly payment is " + loanBalance * interestQuotient
+};
